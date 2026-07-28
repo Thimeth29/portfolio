9f95278d697b14f7e6b4e438f4217652c9657ae9
Your Name <your.email@example.com>
2026-07-28 18:29:14 +0530

    Add assetPrefix to fix subdirectory styling paths

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,8 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  // If your GitHub repository is named 'portfolio', uncomment the next line:
-  // basePath: "/portfolio",
+  basePath: "/portfolio",
+  assetPrefix: "/portfolio/",
 };
 
 export default nextConfig;
